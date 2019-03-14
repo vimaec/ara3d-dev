2b22e2e66241468fcd00ec0e5257a43a656d53f1
FrozenKiwi <stephen.taylor.dev@gmail.com>
2019-03-14 11:10:15 -0400

    Basic concept commit

--- dotnet/AraGeometry/GeometryReader.cs
+++ dotnet/AraGeometry/GeometryReader.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using Ara3D.DotNetUtilities;
 using Newtonsoft.Json.Linq;
 
 namespace Ara3D
@@ -11,8 +14,8 @@ namespace Ara3D
         public static IGeometry ReadG3D(string filePath)
             => G3DExtensions.ReadFromFile(filePath).ToIGeometry();
 
-        public static IList<ManifestSceneNode> ReadManifest(string filePath) 
-           => Util.LoadJsonArray(filePath).ToObject<IList<ManifestSceneNode>>();
+        public static IList<ManifestSceneNode> ReadManifest(string manifestJson)
+           => JArray.Parse(manifestJson).ToObject<IList<ManifestSceneNode>>();
 
         public static IArray<ISceneNode> ManifestNodesToSceneNodes(this IList<ManifestSceneNode> manifest)
             => manifest.Select(
@@ -20,59 +23,44 @@ namespace Ara3D
                     node.ElementId, node.MaterialId, node.CategoryId) as ISceneNode).ToIArray();
 
         /// <summary>
-        /// Loads a scene from a folder containing G3D files and a manifest. 
+        /// Loads a scene from a folder containing G3D files and a manifest.
+        /// Enumerator returns an IScene on load completion
         /// </summary>
-        public static IScene ReadScene(string folder)
+        public static async Task<IScene> ReadScene(IResourceLoader loader)
         {
-            var manifestFile = Path.Combine(folder, "manifest.json");
-            if (!File.Exists(manifestFile))
-                throw new Exception($"Could not find manifest.json file in folder {folder}");
+            // We wait while the loader gets us our resources
+            if (!await loader.ResourceManifest(out var data))
+                return null;
 
-            var nodes = ReadManifest(manifestFile);
-            var files = Directory.GetFiles(folder, @"*.g3d");
-
-            // Load the G3D files in parallel and put in a lookup table
-            var geometries = files.AsParallel().ToDictionary(
-                f => int.Parse(Path.GetFileNameWithoutExtension(f) ?? throw new InvalidOperationException()),
-                ReadG3D);
-
-            // Check that all expected geometries are present (0..n-1)
-            for (var i=0; i < geometries.Count; ++i)
-                if (!geometries.ContainsKey(i))
-                    throw new Exception($"Could not find expected geometry index {i}");
-
-            // Check that all referenced geometries in the manifest are present 
-            foreach (var n in nodes)
-                if (!geometries.ContainsKey(n.GeometryId))
-                    throw new Exception($"Could not find geometry {n.GeometryId}.g3d in folder {folder}");
-                      
-            // Get the geometries in order 
-            var orderedGeometries = geometries.OrderBy(kv => kv.Key).Select(kv => kv.Value);
-
-            // Create the scene 
-            return new Scene(orderedGeometries.ToIArray(), nodes.ManifestNodesToSceneNodes());
+            // Our load is lazy, which means we only load the bare
+            // minimum by default.  Further loads (eg geometry, BIM data)
+            // will trigger the fetching of the data required
+            var nodes = ReadManifest(data);
+            return new Scene(nodes.ManifestNodesToSceneNodes(), loader);
         }
 
+        // A BFast is just a special type of resource loader
         /// <summary>
         /// Loads a scene embedded in a BFAST package.
         /// </summary>
-        public static IScene ReadSceneFromBFast(string filePath)
-        {
-            var bFast = BFastExtensions.ReadBFast(filePath);
+        //public static IScene ReadSceneFromBFast(string filePath)
+        //{
+        //    var bFast = BFastExtensions.ReadBFast(filePath);
 
-            var manifestBuffer = bFast.Buffers[0];
+        //    var manifestBuffer = bFast.Buffers[0];
 
-            // TODO: converting to a string from a span would be more efficient
-            var manifestText = manifestBuffer.ToBytes().ToUtf8();
+        //    // TODO: converting to a string from a span would be more efficient
+        //    var manifestText = manifestBuffer.ToBytes().ToUtf8();
 
-            // TODO: the G3D create here is doing a copy under the hood, which sucks
-            var geometries = bFast.Buffers.Skip(1).AsParallel().AsOrdered().Select(b => G3D.Create(b).ToIGeometry()).ToList();
+        //    // TODO: the G3D create here is doing a copy under the hood, which sucks
+        //    var geometries = bFast.Buffers.Skip(1).AsParallel().AsOrdered().Select(b => G3D.Create(b).ToIGeometry()).ToList();
 
-            // Get the scene nodes from the manifest 
-            var manifest = JArray.Parse(manifestText).ToObject<IList<ManifestSceneNode>>();
+        //    // Get the scene nodes from the manifest 
+        //    var manifest = JArray.Parse(manifestText).ToObject<IList<ManifestSceneNode>>();
 
-            // Creates a scene API
-            return new Scene(geometries.ToIArray(), manifest.ManifestNodesToSceneNodes());
-        }
+        //    // Creates a scene API
+        //    throw new ApplicationException();
+        //    //return new Scene(manifest.ManifestNodesToSceneNodes(), null);
+        //}
     }
 }
---
+++ dotnet/Utilities/FilesystemLoader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ara3D.DotNetUtilities
+{
+    /// <summary>
+    /// Sample implementation of IResourceLoader that loads directly from
+    /// the file system.
+    /// Note the methods are actually synchronous despite returning Task
+    /// </summary>
+    class FilesystemLoader : IResourceLoader
+    {
+        private string _rootPath;
+
+        public void SetRootUri(Uri root)
+        {
+            _rootPath = root.LocalPath;
+        }
+
+        public Task<bool> ResourceManifest(out string manifest)
+        {
+            return ResourceString(new Uri("manifest.json"), out manifest);
+        }
+
+        public Task<bool> ResourceBytes(Uri resourceUri, out byte[] data)
+        {
+            var path = Path.Combine(_rootPath, resourceUri.LocalPath);
+            try
+            {
+                data = File.ReadAllBytes(path);
+                return Task.FromResult(true);
+            }
+            catch (Exception e)
+            {
+                // TODO: something with e
+                data = null;
+                return Task.FromResult(false);
+            }  
+        }
+
+        public Task<bool> ResourceString(Uri resourceUri, out string data)
+        {
+            var path = Path.Combine(_rootPath, resourceUri.LocalPath);
+            try
+            {
+                data = File.ReadAllText(path);
+                return Task.FromResult(true);
+            }
+            catch (Exception e)
+            {
+                // TODO: something with e
+                data = null;
+                return Task.FromResult(false);
+            }
+        }
+
+        public Task<bool> RequestAvailable(out Uri[] available)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RequestAvailable(Uri path, out Uri[] available)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
