049fb70a5b7bb774ebab12921caf70d98a6bfdce
Luke Mamacos <lnmamacos@hotmail.com>
2019-05-27 14:32:08 +0200

    Added FXB Importer test app

---
+++ dotnet/FbxImporterTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using FbxClrWrapper;
+using System.Collections.Generic;
+
+namespace Ara3D
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var filePath = @"E:\VimAecDev\vims\Models\MobilityPavilion_mdl.fbx";
+
+            Ara3D.FbxImporter.CreateScene("");
+            var scene = FbxImporter.CreateScene(filePath);
+            var outputFilePath = @"d:\test.obj";
+            scene.ToIGeometry().WriteObj(outputFilePath);
+        }
+    }
+}
